093f758f4df8b639a02ae6b268963806a8f7e2d4
itqptzadm <itqptzadm@gmail.com>
2018-09-17 17:46:07 +0300

    second commit

--- src/Main.py
+++ src/Main.py
@@ -1,24 +1,22 @@
-#this is firs commit
+#this is second commit
 
+import psutil
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import time
 
-fromaddr = "frommail@gmail.com"
-mypass = "testpswd"
-toaddr = "tomail@yandex.ru"
+print(psutil.sensors_battery().percent)
 
-msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "Привет от питона"
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 
-body = "Это пробный текст сообщения"
-msg.attach(MIMEText(body, 'plain'))
+smtpObj.starttls()
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(fromaddr, mypass)
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
-server.quit()
+smtpObj.login('frommail@gmail.com','testpswd')
+
+smtpObj.sendmail("frommail@gmail.com","tomail@yandex.ru","Program start!")
+
+while True:
+    print("I work")
+    time.sleep(5)   # Delay
+    if psutil.sensors_battery().percent<99:
+        myMassage = "Now battary is less than " + str(psutil.sensors_battery().percent)
+        print("I send")
